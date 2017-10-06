d108b6f05ef1b968392d9274e80c0d3a07949815
Scott Cytacki <scytacki@concord.org>
2017-10-06 11:09:34 -0400

    update readme and update comments in the code

--- thermoscope-bluefruit.ino
+++ thermoscope-bluefruit.ino
@@ -20,12 +20,12 @@ The bluetooth code was copied from their healththermometer example.
 #include "BluefruitConfig.h"
 
 // which analog pin to connect A sensor
-#define THERMISTORPINA A2         
+#define THERMISTORPINA A2
 // which analog pin to connect B sensor
-#define THERMISTORPINB A5         
+#define THERMISTORPINB A5
 
 // temp. for nominal resistance (almost always 25 C)
-#define TEMPERATURENOMINAL 25   
+#define TEMPERATURENOMINAL 25
 // how many samples to take and average, more takes longer
 // but is more 'smooth'
 #define NUMSAMPLES 80
@@ -46,7 +46,7 @@ int samples[NUMSAMPLES];
 /* Create the bluefruit object, if you want to use this sketch with a different bluefruit
  *  module configuration see the bluefruit examples for the different configurations.
  *  This is specific to the bluefruit feather:
- *  hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST 
+ *  hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST
  */
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
@@ -112,6 +112,11 @@ void BleGattRX(int32_t chars_id, uint8_t data[], uint16_t len)
   callback(chars_id, data, len);
 }
 
+/*
+  Handle an incoming id string. This is string is saved in
+  the bluefruits non volitale memory, and it used as a suffixed to
+  "Thermoscope " to make it easier to identify the device.
+*/
 void BleGattRXId(int32_t chars_id, uint8_t data[], uint16_t len)
 {
   Serial.print( F("[BLE GATT RX] id:" ) );
@@ -119,18 +124,20 @@ void BleGattRXId(int32_t chars_id, uint8_t data[], uint16_t len)
   Serial.print(F(", len: "));
   Serial.print(len);
   Serial.print(F(" "));
-  
+
   if (chars_id == idStringCharId)
-  {   
+  {
     Serial.write(data, len);
     Serial.println();
     // save this into NVM
 
     // the incoming data buffer is not null terminated.
-    // so clear out the idString first before copying the chars
+    // set the string to be all 0s before copying in new characters
     memset(userConfigV1.idString, 0, ID_STRING_LEN);
 
-    // make sure we leave at least one /0 at the end of the string
+    // leave at least one blank byte at the end of the idString this way at least
+    // one of the 0s that we added before will be at the end of the string.
+    // this way it should guaruntee the string is NULL terminated
     strncpy(userConfigV1.idString, (char *)data, min(len,(ID_STRING_LEN - 1)));
     updateNVM();
 
@@ -139,7 +146,7 @@ void BleGattRXId(int32_t chars_id, uint8_t data[], uint16_t len)
   }
 }
 
-void BleGattRXSensor(SensorServiceInfo *sensorService, TempCoefficients *coefficients, 
+void BleGattRXSensor(SensorServiceInfo *sensorService, TempCoefficients *coefficients,
                      int32_t chars_id, uint8_t data[], uint16_t len)
 {
   Serial.print( F("[BLE GATT RX] id:" ) );
@@ -177,16 +184,16 @@ size_t idLength() {
 
 int32_t addIntegerOutputCharacteristic(uint16_t uuid) {
   int32_t id = gatt.addCharacteristic(
-    uuid, 
+    uuid,
     GATT_CHARS_PROPERTIES_READ | GATT_CHARS_PROPERTIES_NOTIFY, // read and notify
     4, // min len
     4, // max len
-    BLE_DATATYPE_BYTEARRAY); // tried using integer here but underlying gatt library always 
+    BLE_DATATYPE_BYTEARRAY); // tried using integer here but underlying gatt library always
                              // uses bytearray which then caused an error when calling writeChar
-                             
+
   if (id == 0) {
     error(F("Could not add characteristic"));
-  }  
+  }
 
   return id;
 }
@@ -199,9 +206,9 @@ int32_t addCoefficientCharacteristic(uint16_t uuid, uint16_t value, GattCallback
     GATT_CHARS_PROPERTIES_WRITE | GATT_CHARS_PROPERTIES_READ,
     2, // min len
     2, // max len
-    BLE_DATATYPE_BYTEARRAY); // tried using integer here but underlying gatt library always 
+    BLE_DATATYPE_BYTEARRAY); // tried using integer here but underlying gatt library always
                              // uses bytearray which then caused an error when calling writeChar
-                             
+
   if (id == 0) {
     error(F("Could not add characteristic"));
   }
@@ -214,7 +221,7 @@ int32_t addCoefficientCharacteristic(uint16_t uuid, uint16_t value, GattCallback
   return id;
 }
 
-void addSensorService(uint8_t serviceUUID[], SensorServiceInfo *info, TempCoefficients *coefficients, 
+void addSensorService(uint8_t serviceUUID[], SensorServiceInfo *info, TempCoefficients *coefficients,
   GattCallback gattCallback){
   /* Service ID should be 1 */
   // TODO see if we avoid this duplicate UUID def to prevent a mistake
@@ -224,7 +231,7 @@ void addSensorService(uint8_t serviceUUID[], SensorServiceInfo *info, TempCoeffi
   if (info->serviceId == 0) {
     error(F("Could not add service"));
   }
-  
+
   // Add the Temperature Measurement characteristic which is an int with the temp in C times 100
   // so a temperatue of 30.01 which will be sent as 3001
   Serial.println(F("Adding the Temperature Measurement characteristic (UUID = 0x0001):"));
@@ -239,17 +246,17 @@ void addSensorService(uint8_t serviceUUID[], SensorServiceInfo *info, TempCoeffi
 
   // Add Series resistor characteristic an int with the resistance in Ohms.
   Serial.println(F("Adding the Series characteristic (UUID = 0x0003):"));
-  info->seriesResistanceCharId = 
+  info->seriesResistanceCharId =
     addCoefficientCharacteristic(0x0003, coefficients->seriesResistance, gattCallback);
 
   // Add thermistor nominal resistance characteristic an int with the resistance in Ohms.
   Serial.println(F("Adding the ThermistorNominalResistance characteristic (UUID = 0x0004):"));
-  info->thermistorNominalResistanceCharId = 
+  info->thermistorNominalResistanceCharId =
     addCoefficientCharacteristic(0x0004, coefficients->thermistorNominalResistance, gattCallback);
-    
+
   // Add thermistor beta characteristic an int.
   Serial.println(F("Adding the ThermistorBeta characteristic (UUID = 0x0005):"));
-  info->thermistorBetaCharId = 
+  info->thermistorBetaCharId =
     addCoefficientCharacteristic(0x0005, coefficients->thermistorBeta, gattCallback);
 }
 
@@ -311,8 +318,14 @@ void setup(void) {
   char identifier[5];
   ble.readNVM(0, &magic_number);
 
+
   if ( magic_number != MAGIC_NUMBER )
   {
+    // We have been having an issues with thermoscopes losing their name
+    // If the readNVM call above didn't work, then we do a reset. Perhaps
+    // sometimes the readNVM fails even though the data is still there
+    // so it might be good to look at the return value of readNVM and try again
+
     /* Perform a factory reset to make sure everything is in a known state */
     Serial.println(F("Magic not found: performing a factory reset: "));
     if ( ! ble.factoryReset() ){
@@ -329,22 +342,18 @@ void setup(void) {
   }else
   {
     Serial.println(F("Magic found"));
-    // I want to read in a utf8 string here it could be a variable length
-    // Lets try giving it 5 bytes and see what the various string functions do with it if it is
-    // longer
     int32_t nvmVersion;
     ble.readNVM(4, &nvmVersion);
-    // Write data to NVM
     Serial.print( F("NVM Version: ") );
     Serial.println(nvmVersion);
 
     ble.readNVM(8, (uint8_t *)&userConfigV1, sizeof(UserConfigV1));
 
-    // make sure we don't override the buffer incase the string is not null terminated
     size_t _idLength = idLength();
     Serial.print( F("Length of id char: "));
     Serial.println(_idLength);
     Serial.println(userConfigV1.idString);
+    // make sure we don't override the buffer incase the string is not null terminated
     if(_idLength > 0){
       strcat_P(device_name, PSTR(" "));
       // we've made sure idLength is not greater than 4 so this way we shouldn't override
@@ -371,19 +380,18 @@ void setup(void) {
 
   // add generic thermoscope service
   // add writable characteristic for the identifier
-  // add readable characteristic for version string
+  // TODOL add readable characteristic with a version
   gatt.addService(0x1234);
 
   // The AT command has the ability to set a default value but this helper function doesn't
-  idStringCharId = gatt.addCharacteristic(0x2345, GATT_CHARS_PROPERTIES_WRITE | GATT_CHARS_PROPERTIES_READ, 
+  idStringCharId = gatt.addCharacteristic(0x2345, GATT_CHARS_PROPERTIES_WRITE | GATT_CHARS_PROPERTIES_READ,
     1, 5, BLE_DATATYPE_STRING, "identifier char");
 
-  // set the default value, I'm hoping I can do this before doing the reset
-  // this is assuming the string is always null terminated. That should be the case if the userConfig
+  // set the default value of the idString
+  // this assuming the string is always null terminated. That should be the case if the userConfig
   // was initialized correctly.
-  // There is something strange here, if the type of the characteristic is set to string, and then we
-  // try to update it using byte array, then the value gets set to the hex form of the of the string
-  // for exampe '68-69' instead of 'hi'
+  // Note: the idString is a char[].  If a its type was a uint8_t[] then the setChar method trys to do
+  // a conversion so if the idString was intended to be 'h', the actual value saved would be '68'
   gatt.setChar(idStringCharId, userConfigV1.idString);
 
   // add a callback so we get notified when this changes
@@ -392,11 +400,11 @@ void setup(void) {
   // TODO: Using the autogenerated one below didn't work for some reason???
   // {0xbc, 0x44, 0x00, 0x00, 0xf2, 0x44, 0x4f, 0x49, 0xb6, 0x56, 0xdf, 0x49, 0x85, 0x77, 0x55, 0xe4 };
   // So instead we are using some other vendor UUIDs that I know work
-  uint8_t sensorAServiceUUID[] = 
+  uint8_t sensorAServiceUUID[] =
     {0xf0, 0x00, 0xaa, 0x00, 0x04, 0x51, 0x40, 0x00, 0xb0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
   addSensorService(sensorAServiceUUID, &sensorServiceA, &userConfigV1.sensorA, BleGattRXSensorA);
-    
-  uint8_t sensorBServiceUUID[] = 
+
+  uint8_t sensorBServiceUUID[] =
     {0xf0, 0x00, 0xbb, 0x00, 0x04, 0x51, 0x40, 0x00, 0xb0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
   addSensorService(sensorBServiceUUID, &sensorServiceB, &userConfigV1.sensorB, BleGattRXSensorB);
 
@@ -405,7 +413,7 @@ void setup(void) {
 
   // Enable Battery service do not reset, we'll do that ourselves
   battery.begin(false);
-  
+
   /* Reset the device for the new service setting changes to take effect */
   Serial.print(F("Performing a SW reset (service changes require a reset): "));
   ble.reset();
@@ -423,7 +431,7 @@ void readTemperature(SensorServiceInfo *sensorService, const __FlashStringHelper
   for (i=0; i< NUMSAMPLES; i++) {
    samples[i] = analogRead(pin);
   }
- 
+
   // average all the samples out
   averageDigitialCounts = 0;
   for (i=0; i< NUMSAMPLES; i++) {
@@ -436,19 +444,19 @@ void readTemperature(SensorServiceInfo *sensorService, const __FlashStringHelper
   int32_t microVolts = (int32_t)((averageDigitialCounts / 4095) * 3300000);
   gatt.setChar(sensorService->microVoltsCharId, microVolts);
 
-  // Serial.print("Average analog reading "); 
+  // Serial.print("Average analog reading ");
   // Serial.println(average);
- 
+
   // convert the value to resistance
   // Solving the voltage divider equation: Vout = Vin * R2 / (R1 + R2)
   // For R2 yields: R2 = R1 / ((Vin / Vout) - 1)
-  
-  float resistanceRatio = (4095 / averageDigitialCounts) - 1;  
+
+  float resistanceRatio = (4095 / averageDigitialCounts) - 1;
   float resistance = coefficients->seriesResistance  / resistanceRatio;
 
-  // Serial.print("Thermistor resistance "); 
+  // Serial.print("Thermistor resistance ");
   // Serial.println(average);
- 
+
   float steinhart;
   steinhart = resistance / coefficients->thermistorNominalResistance;     // (R/Ro)
   steinhart = log(steinhart);                                          // ln(R/Ro)
@@ -456,23 +464,23 @@ void readTemperature(SensorServiceInfo *sensorService, const __FlashStringHelper
   steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15);                    // + (1/To)
   steinhart = 1.0 / steinhart;                                         // Invert
   steinhart -= 273.15;                         // convert to C
- 
-  Serial.print(F("Temperature ")); 
-  Serial.print(label); 
-  Serial.print(F(" ")); 
+
+  Serial.print(F("Temperature "));
+  Serial.print(label);
+  Serial.print(F(" "));
   Serial.print(steinhart);
   Serial.println(F(" *C"));
 
   // convert temperature to 32bit integer with 2 decimals of precision
   // need to verify that negative numbers work
   int32_t temperature_100 = (int32_t)(steinhart * 100);
-  Serial.print(F("Temperature*100 ")); 
+  Serial.print(F("Temperature*100 "));
   Serial.println(temperature_100);
-  
+
   gatt.setChar(sensorService->measureCharId, (int32_t)(steinhart * 100));
 
 }
- 
+
 void loop(void) {
   uint8_t i;
   float average;
@@ -481,7 +489,7 @@ void loop(void) {
   // only used for the name characteristic, but we could use it for the isConnected as well
   // and better yet we could use the DFU IRQ approach so no polling of the module is necessary
   ble.update(2000);
-  
+
   // This is useful for testing the reading and writing of characteristics without all of the other
   // info printed in the console
   //  if (1) {
@@ -498,7 +506,7 @@ void loop(void) {
 
       // We might be able to use the DFU interrupt mode so we are woken up by the BLE when there is
       // a connection event. This would save power, when we are not connected.
-      
+
       // Seems the best option I can find for sleep is using rtc standby:
       // https://github.com/cavemoa/Feather-M0-Adalogger/blob/master/SimpleSleepUSB/SimpleSleepUSB.ino
     }
@@ -509,7 +517,7 @@ void loop(void) {
 
   // if we are here then we are connected
   disconnectedCount = 0;
- 
+
   // Update battery reading
   float measuredvbat = analogRead(VBATPIN);
   measuredvbat *= 2;    // we divided by 2, so multiply back
@@ -517,11 +525,11 @@ void loop(void) {
   measuredvbat /= 4096; // convert to voltage in 12bit mode
   // the voltage should range between 3 and 4.2 (but might go higher)
   uint8_t percentage = (uint8_t)((measuredvbat - 3.0) / 1.25 * 100);
-  
+
   Serial.print("Update battery level voltage: ");
   Serial.print(measuredvbat);
   Serial.print(" percent: ");
-  Serial.println(percentage);  
+  Serial.println(percentage);
   battery.update(percentage);
 
   readTemperature(&sensorServiceA, F("A"), THERMISTORPINA, &userConfigV1.sensorA);
